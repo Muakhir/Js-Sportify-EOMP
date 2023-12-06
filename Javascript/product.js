f1621c83ad5218c778b268eb466bbaee6fdca060
Muakhir <almuakhirjones@gmail.com>
2023-12-06 11:50:43 +0200

    Product/Sort/Search

--- Javascript/product.js
+++ Javascript/product.js
@@ -5,6 +5,7 @@ new Date().getFullYear()
 let productsWrapper = document.querySelector('[data-items]')
 let searchProduct = document.querySelector('[data-input]')
 let productSort = document.querySelector('[data-sort]')
+let Checkout = []
 let Cart = document.querySelector('[data-cart]')
 let items = JSON.parse( localStorage.getItem('items')) ? JSON.parse( localStorage.getItem('items'))
 : localStorage.setItem('items',JSON.stringify(
@@ -12,7 +13,7 @@ let items = JSON.parse( localStorage.getItem('items')) ? JSON.parse( localStorag
         {
             id: 1,
             Name: 'Man City Kit',
-            Amount:"R999.99",
+            Amount: 999.99,
             Image: "https://i.postimg.cc/Fs3FmYJ3/Manchester-City-Home-Player-Version-Football-Shirt-23-24.jpg",
             
         },
@@ -20,53 +21,49 @@ let items = JSON.parse( localStorage.getItem('items')) ? JSON.parse( localStorag
             id: 2,
             Name: 'Tottenham Hotspur Kit',
             Image: "https://i.postimg.cc/xd0LLDSp/Sqd8-Blb-KOf-UAk-HU-2fec0810-1be3-4af9-8ea1-0918fe2d1d04-540x.jpg",
-            Amount:"R1199.99"
+            Amount: 1199.99
         },
         {
             id: 3,
             Name: 'Chelsea Kit',
             Image: "https://i.postimg.cc/zDxjFkDX/FBsy-UIAe-RHW.png",
-            Amount:"R1400.00"
+            Amount: 1400.00
         },
         {
             id: 4,
             Name: 'Man United Kit',
             Image: "https://i.postimg.cc/8c3qrmJw/IP1739-03-laydown.jpg",
-            Amount:"R1300.00"
+            Amount: 1300.00
         },
         {
             id: 5,
             Name: 'Liverpool Kit',
             Image: "https://i.postimg.cc/HLqNJdSf/1165863-REDWHITE-1-v1-6-Fk.jpg",
-            Amount:"R1299.99"
+            Amount: 1299.99
         },  {
             id: 6,
             Name: 'Arsenal Kit',
             Image: "https://i.postimg.cc/V616cry6/image.png",
-            Amount: "R1699.99"
+            Amount: 1699.99
         }
     ]
     ))
     function displayProduct(){
         productsWrapper.innerHTML = ''
-        try{
-            if(items){
+            if(items.length){
                 items.forEach( items =>{
                     productsWrapper.innerHTML += `<div class="card" style="width: 20rem;">
                     <img src="${items.Image}" class="card-img-top pimg" alt="${items.id}">
                     <div class="card-body">
                     <h5 class="card-title">${items.Name}</h5>
-                    <p class="card-text">${items.Amount}</p>
-                    <button class="btn btn-primary">Cart</button>
+                    <p class="card-text">R${items.Amount}</p>
+                    <button class="btn btn-primary" onclick='addCart(${JSON.stringify(items)})'>Cart</button>
                     </div>
                     </div>`
                 })
             }else {
                 productsWrapper.innerHTML = 'No Item Was Found'
             }
-        }catch(e){
-            console.log(e.message)
-        }
     }
     displayProduct()
 
@@ -75,45 +72,39 @@ let items = JSON.parse( localStorage.getItem('items')) ? JSON.parse( localStorag
             let searchproduct = items.filter( prod =>{
                 return prod.Name.toLowerCase().includes(searchProduct.value.toLowerCase())
             })
-            if(searchProduct.length != 0){
+            if(searchproduct.length){
                 productsWrapper.innerHTML = ''
                 searchproduct.forEach(items =>{
                 productsWrapper.innerHTML += `<div class="card" style="width: 20rem;">
                     <img src="${items.Image}" class="card-img-top pimg" alt="${items.id}">
                     <div class="card-body">
                     <h5 class="card-title">${items.Name}</h5>
-                    <p class="card-text">${items.Amount}</p>
-                    <button class="btn btn-primary">Cart</button>
+                    <p class="card-text">R${items.Amount}</p>
+                    <button class="btn btn-primary" onclick='addCart(${JSON.stringify(items)})'>Cart</button>
                     </div>
                     </div>`
                 })
             }else{
-                productsWrapper.innerHTML = 'No Items Found'
+                productsWrapper.innerHTML = "Not Found"
+                
             }
         }catch(e){
             console.log(e.message);
         }
     })
     
+    productSort.addEventListener('click',()=>{
+        items.sort((Arg1 , Arg2) => {
+                return Arg1.Amount - Arg2.Amount 
+            })
+            displayProduct()
+    })
 
-    function sortItems(){
-    items.sort((a , b) => {
-            return a.Name.localeCompare(b.Name)
-        });
-        displayProduct()
+    function addCart(items) {
+       if(items){
+        Checkout.push(items)
+        localStorage.setItem('purchased', JSON.stringify(Checkout))
+
+       }
     }
-    
-    productSort.addEventListener('click',sortItems)
 
-// Assuming Cart and items are defined somewhere in your code
-
-function addCartItem(i) {
-    Cart.push(items[i])
-    localStorage.setItem('purchased', JSON.stringify(Cart))
-}
-document.addEventListener('click', function(event) {
-    if (event.target.hasAttribute('data-cart')) {
-        addCartItem(event.target.value)
-    }
-    displayProduct()
-});
