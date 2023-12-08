cf1596ea8feb64a1ca2b2511b9a42e4a3ae7beb0
Muakhir <almuakhirjones@gmail.com>
2023-12-08 12:41:19 +0200

    Admin/AddItems

--- Javascript/admin.js
+++ Javascript/admin.js
@@ -31,26 +31,27 @@ function displayAdmin(){
                               </div>
                               <div class="modal-body">
                               <div class="col">
-                                <label for="recipient-name${i}" class="col-form-label">Name:</label>
-                                <input type="text" class="form-control" id="recipient-Name${i}" value="${items.Name}">
+                                <label for="Item-Name${i}" class="col-form-label">Name:</label>
+                                <input type="text" class="form-control" id="Item-Name${i}" value="${items.Name}">
                                 </div>
                                 <div class="col">
-                                <label for="recipient-name${i}" class="col-form-label">Id:</label>
-                                <input type="text" class="form-control" id="recipient-id${i}" value="${items.id}">
+                                <label for="Item-id${i}" class="col-form-label">Id:</label>
+                                <input type="text" class="form-control" id="Item-id${i}" value="${items.id}">
                                 </div>
                                 <div class="col">
-                                <<label for="recipient-name${i}" class="col-form-label">Price:</label>
-                                <input type="text" class="form-control" id="recipient-Amount${i}" value="R${items.Amount}">
+                                <label for="Item-Amount${i}" class="col-form-label">Price:</label>
+                                <input type="text" class="form-control" id="Item-Amount${i}" value="${items.Amount}">
                                 </div>
                                 <div class="col">
-                                <label for="recipient-name${i}" class="col-form-label">Image:</label>
-                                <input type="text" class="form-control" id="recipient-Image${i}" value="${items.Image}">
+                                <label for="Item-Image${i}" class="col-form-label">Image:</label>
+                                <input type="text" class="form-control" id="Item-Image${i}" value="${items.Image}">
                                 </div>
                                 
                               </div>
                               <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
+                                <button type="button" class="btn btn-primary" onclick='new UpdateItem(${JSON.stringify(items)}, 
+                                ${JSON.stringify(i)})'>Save changes</button>
                               </div>
                             </div>
                           </div>
@@ -83,5 +84,14 @@ function deleteItem(index) {
     displayAdmin()
 }
 
-
+function UpdateItem(items,i){
+  this.id = items.id 
+  this.Name = document.querySelector(`#Item-Name${i}`).value
+  this.Amount = document.querySelector(`#Item-Amount${i}`).value
+  this.Image = document.querySelector(`#Item-Image${i}`).value
+  admin[i] = Object.assign({}, this)
+  localStorage.setItem('items', JSON.stringify(admin))
+  displayAdmin()
+  location.reload()
+}
 
